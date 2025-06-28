b22ca8cafdddb6c5d1eb1847668d39e88673f5fe
Om Zalavadiya <omzalavadiya301002@gmail.com>
2025-06-28 17:26:04 +0530

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Low' }
+  priority: { type: String, enum: ['Low', 'Medium', 'Urgent'], default: 'Low' }
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
